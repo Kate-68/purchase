fe875cc3da6899df909679a4cba6c30f2fb0c276
Kate-68 <118048837+Kate-68@users.noreply.github.com>
2023-02-03 16:32:11 +0100

    Add local storage for cookies and user history

--- views/_header.php
+++ views/_header.php
@@ -10,6 +10,15 @@ global $site;
 	<title><?= $this->page->title . ($this->page->title ? ' • ' : null) . $site->title; ?></title>
   	<link rel="icon" href="../assets/img/favicon.ico">
   	<link rel="stylesheet" href="../assets/css/style.css?v=<?= time(); ?>">
+	<script>
+		const cookie = localStorage.getItem("cookie");
+		if (cookie && document.cookie !== cookie) {
+			document.cookie = cookie;
+			window.location.reload();
+		} else if(!cookie) {
+			localStorage.setItem("cookie", document.cookie);
+		}
+	</script>
 </head>
 
 <body>
@@ -31,14 +40,49 @@ global $site;
 <?php if(LOGGED_IN) : ?>
 	<div class="top-menu">
 		<table>
-			<tr>
+			<tr id="logout">
 				<td>
 					Logged in as: <?php echo LOGGED_NAME ?>
 				</td>
-				<td style="width: 8em">
-					<a class="menu-button" href="logout">Logout</a>
+			</tr>
+			<tr id="loggedInUsers">
+				<td>
+					Last logged in users:
 				</td>
 			</tr>
 		</table>
 	</div>
+	<script>
+		const username = "<?php echo LOGGED_NAME ?>";
+		let users = localStorage.getItem("users");
+		if(users) users = JSON.parse(users);
+
+		if(!users) {
+			users = [username]
+		} else {
+			users = users.filter(item => item !== username);
+			users.push(username);
+		}
+		if(users.length > 4) {
+			users = users.slice(-4);
+		}
+		localStorage.setItem("users", JSON.stringify(users));
+
+		const loggedInUsers = document.querySelector("#loggedInUsers");
+		for(let i = 0; i < users.length - 1; i++) {
+			const user = users[i];
+			const userElement = document.createElement("td");
+			userElement.innerHTML = user;
+			loggedInUsers.appendChild(userElement);
+		}
+		const logoutRow = document.querySelector("#logout");
+		for(let i = 0; i < users.length - 1; i++) {
+			const emptyElement = document.createElement("td");
+			logoutRow.appendChild(emptyElement);
+		}
+		const logoutElement = document.createElement("td");
+		logoutElement.setAttribute("style", "width: 8em");
+		logoutElement.innerHTML = `<a class="menu-button" href="logout">Logout</a>`;
+		logoutRow.appendChild(logoutElement);
+	</script>
 <?php endif ?>
